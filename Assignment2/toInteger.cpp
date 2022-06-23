eb1523322d5dc9384c51de2b907f7929cf85b56f
Chi <chiemi2400@gmail.com>
2022-06-22 22:28:25 -0700

    Update toInteger.cpp

--- Assignment2/toInteger.cpp
+++ Assignment2/toInteger.cpp
@@ -1,3 +1,11 @@
+/**
+** Program Name: Recursive insertion program:
+** Purpose: Implements recursive and iterative functions that converts a string into an integer.
+** Author: Chiemi Mita
+** Limitation: No known limitations
+*/
+
+
 #include<iostream>
 using namespace std;
 
@@ -11,15 +19,20 @@ int main(){
     return 0;
 }
 
+//Recursive function
 int toInteger(const string& str){
     return toInteger(str, str.size()-1);
 }
+
+//Overload toInteger function
 int toInteger(const string& str, int high){
     if (high==0)
         return str[high]-'0';
 	return toInteger(str, high-1) * 10 + (str[high] - '0');
 
 }
+
+//Iterative function
 int toIntegerIterative(const string& str){
     int result=0;
     for (int i=0; i<str.size(); i++)
